01806f7914d7f69b855394f69722df2423cc70ed
manoelvsalgado <manoelsalgadoneto@gmail.com>
2026-03-10 17:15:38 -0300

    fix: remove language detection configuration from i18n setup

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,21 +1,16 @@
 import i18n from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 import { resources } from './locales/resources';
 
 void i18n
-  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources,
+    lng: 'pt',
     fallbackLng: 'pt',
     supportedLngs: ['pt', 'en', 'es'],
     interpolation: {
       escapeValue: false,
-    },
-    detection: {
-      order: ['localStorage', 'navigator', 'htmlTag'],
-      caches: ['localStorage'],
     },
   });
 
